edd50d1e0e2a68b677800c724e3905689e45e0e6
Fabricio Carvalho <fcarvalho1999@gmail.com>
2022-01-31 19:43:58 -0300

    color change

--- src/components/AlphabetDrinks.vue
+++ src/components/AlphabetDrinks.vue
@@ -1,21 +1,34 @@
 <template>
   <div id="letter-search" class="w-full px-8">
-    <h3 class="text-2xl text-center">Browse Drinks<br /></h3>
+    <h3 class="text-2xl font-bold text-center">Browse Drinks<br /></h3>
     <br />
     <div class="text-center">
-      <a href="/browse.php?b=a">A</a> / <a href="/browse.php?b=b">B</a> /
-      <a href="/browse.php?b=c">C</a> / <a href="/browse.php?b=d">D</a> /
-      <a href="/browse.php?b=e">E</a> / <a href="/browse.php?b=f">F</a> /
-      <a href="/browse.php?b=g">G</a> / <a href="/browse.php?b=h">H</a> /
-      <a href="/browse.php?b=i">I</a> / <a href="/browse.php?b=j">J</a> /
-      <a href="/browse.php?b=k">K</a> / <a href="/browse.php?b=l">L</a> /
-      <a href="/browse.php?b=m">M</a> / <a href="/browse.php?b=n">N</a> /
-      <a href="/browse.php?b=o">O</a> / <a href="/browse.php?b=p">P</a> /
-      <a href="/browse.php?b=q">Q</a> / <a href="/browse.php?b=r">R</a> /
-      <a href="/browse.php?b=s">S</a> / <a href="/browse.php?b=t">T</a> /
-      <a href="/browse.php?b=u">U</a> / <a href="/browse.php?b=v">V</a> /
-      <a href="/browse.php?b=w">W</a> / <a href="/browse.php?b=x">X</a> /
-      <a href="/browse.php?b=y">Y</a> / <a href="/browse.php?b=z">Z</a>
+      <a class="text-pink-400 font-bold" href="/browse.php?b=a">A</a> /
+      <a class="text-pink-400 font-bold" href="/browse.php?b=b">B</a> /
+      <a class="text-pink-400 font-bold" href="/browse.php?b=c">C</a> /
+      <a class="text-pink-400 font-bold" href="/browse.php?b=d">D</a> /
+      <a class="text-pink-400 font-bold" href="/browse.php?b=e">E</a> /
+      <a class="text-pink-400 font-bold" href="/browse.php?b=f">F</a> /
+      <a class="text-pink-400 font-bold" href="/browse.php?b=g">G</a> /
+      <a class="text-pink-400 font-bold" href="/browse.php?b=h">H</a> /
+      <a class="text-pink-400 font-bold" href="/browse.php?b=i">I</a> /
+      <a class="text-pink-400 font-bold" href="/browse.php?b=j">J</a> /
+      <a class="text-pink-400 font-bold" href="/browse.php?b=k">K</a> /
+      <a class="text-pink-400 font-bold" href="/browse.php?b=l">L</a> /
+      <a class="text-pink-400 font-bold" href="/browse.php?b=m">M</a> /
+      <a class="text-pink-400 font-bold" href="/browse.php?b=n">N</a> /
+      <a class="text-pink-400 font-bold" href="/browse.php?b=o">O</a> /
+      <a class="text-pink-400 font-bold" href="/browse.php?b=p">P</a> /
+      <a class="text-pink-400 font-bold" href="/browse.php?b=q">Q</a> /
+      <a class="text-pink-400 font-bold" href="/browse.php?b=r">R</a> /
+      <a class="text-pink-400 font-bold" href="/browse.php?b=s">S</a> /
+      <a class="text-pink-400 font-bold" href="/browse.php?b=t">T</a> /
+      <a class="text-pink-400 font-bold" href="/browse.php?b=u">U</a> /
+      <a class="text-pink-400 font-bold" href="/browse.php?b=v">V</a> /
+      <a class="text-pink-400 font-bold" href="/browse.php?b=w">W</a> /
+      <a class="text-pink-400 font-bold" href="/browse.php?b=x">X</a> /
+      <a class="text-pink-400 font-bold" href="/browse.php?b=y">Y</a> /
+      <a class="text-pink-400 font-bold" href="/browse.php?b=z">Z</a>
     </div>
   </div>
 </template>
