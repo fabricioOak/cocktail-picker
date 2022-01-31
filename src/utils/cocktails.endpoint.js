b3dccf5d4ba344eb6c4762d1e087ce9fcec6ef99
Fabricio Carvalho <fcarvalho1999@gmail.com>
2022-01-31 20:44:15 -0300

    new endpoint

--- src/utils/cocktails.endpoint.js
+++ src/utils/cocktails.endpoint.js
@@ -3,7 +3,8 @@ import BaseEndpoint from './base.endpoint'
 const Cocktails = {
   searchCocktail: (name) => BaseEndpoint.URL + '/search.php?s=' + name,
   searchByLetter: (letter) => BaseEndpoint.URL + '/search.php?f=' + letter,
-  getRandomCocktail: BaseEndpoint.URL + '/random.php'
+  getRandomCocktail: BaseEndpoint.URL + '/random.php',
+  getCocktailDetails: (id) => BaseEndpoint.URL + '/lookup.php?i=' + id
 }
 
 export default Cocktails
