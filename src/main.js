2f2ad2f57e9ad91f915d778ed3f17b6eeb5c1d77
Fabricio Carvalho <fcarvalho1999@gmail.com>
2022-01-30 23:33:35 -0300

    global imports and init

--- src/main.js
+++ src/main.js
@@ -2,12 +2,23 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+
+import Header from './components/Header.vue'
+import AlphabetDrinks from './components/AlphabetDrinks.vue'
+
+import ApiService from './service/api.service'
+
 import './assets/tailwind.css'
+
+ApiService.init()
+
+Vue.component('Header', Header)
+Vue.component('AlphabetDrinks', AlphabetDrinks)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
