ccf68383d7b111fc5a61d1e792188e491a9ecb86
Fabricio Carvalho <fcarvalho1999@gmail.com>
2022-02-01 17:22:31 -0300

    new endpoint

--- src/utils/cocktails.endpoint.js
+++ src/utils/cocktails.endpoint.js
@@ -4,7 +4,8 @@ const Cocktails = {
   searchCocktail: (name) => BaseEndpoint.URL + '/search.php?s=' + name,
   searchByLetter: (letter) => BaseEndpoint.URL + '/search.php?f=' + letter,
   getRandomCocktail: BaseEndpoint.URL + '/random.php',
-  getCocktailDetails: (id) => BaseEndpoint.URL + '/lookup.php?i=' + id
+  getCocktailDetails: (id) => BaseEndpoint.URL + '/lookup.php?i=' + id,
+  searchByUniqueLetter: (letter) => BaseEndpoint.URL + '/search.php?f=' + letter
 }
 
 export default Cocktails
