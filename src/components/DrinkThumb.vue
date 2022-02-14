1d477c84f661440b734cf0547d7b1d0fd3d9f983
Fabricio Carvalho <fcarvalho1999@gmail.com>
2022-02-13 22:04:58 -0300

    change event click

--- src/components/DrinkThumb.vue
+++ src/components/DrinkThumb.vue
@@ -1,20 +1,21 @@
 <template>
-  <div
-    @click="$emit('click')"
-    class="flex flex-col items-center justify-center col-span-1"
-  >
+  <div class="flex flex-col items-center justify-center col-span-1">
     <div class="relative p-5">
       <div
         class="absolute z-10 w-full h-full -mt-5 opacity-30 -ml-5 rounded-full rounded-tr-none"
         :style="{ backgroundColor: this.color }"
       ></div>
       <img
-        class="relative hover:scale-110 scale-100 duration-150 transform h-52 z-20 w-52 rounded-full"
+        @click="$emit('click')"
+        class="cursor-pointer relative hover:scale-110 scale-100 duration-150 transform h-52 z-20 w-52 rounded-full"
         :src="src"
       />
     </div>
     <div class="mt-3 space-y-2 text-center">
-      <div class="space-y-1 text-lg font-medium leading-6">
+      <div
+        @click="$emit('click')"
+        class="cursor-pointer space-y-1 text-lg font-medium leading-6"
+      >
         <h3>{{ name }}</h3>
       </div>
       <div class="relative flex items-center justify-center space-x-3"></div>
